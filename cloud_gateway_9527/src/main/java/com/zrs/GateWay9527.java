7ccd965b839b4c52a138b3db90a204b13b15c0ae
zrsteen <zrsteen@qq.com>
2024-04-21 23:30:49 +0800

    gateway网关

---
+++ cloud_gateway_9527/src/main/java/com/zrs/GateWay9527.java
@@ -0,0 +1,11 @@
+package com.zrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWay9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWay9527.class);
+    }
+}
